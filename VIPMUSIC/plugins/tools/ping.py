24993023c98c6ec9751c7d470095cd6810097f95
Bhadvaheroku <156423343+Bhadvaheroku@users.noreply.github.com>
2024-02-29 12:49:31 +0530

    Update ping.py

--- VIPMUSIC/plugins/tools/ping.py
+++ VIPMUSIC/plugins/tools/ping.py
@@ -71,10 +71,10 @@ async def ping_com(client, message: Message, _):
         ],
         [
             InlineKeyboardButton(
-                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/TG_FRIENDSS",
+                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/deathking_botworld",
             ),
             InlineKeyboardButton(
-                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/VIP_CREATORS",
+                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/DeathxBotz",
             )
         ],
         [
